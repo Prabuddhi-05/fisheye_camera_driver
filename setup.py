19a8c31625ebbd486a3dab3ab00484d07b03af61
Prabuddhi-05 <119497903+Prabuddhi-05@users.noreply.github.com>
2024-07-19 11:08:58 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-          'fisheye_camera_multiple = fisheye_camera_driver.fisheye_camera_multiple:main',
+          'fisheye_camera = fisheye_camera_driver.fisheye_camera:main',
         ],
     },
 )
