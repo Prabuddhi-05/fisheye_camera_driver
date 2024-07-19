e6b057ec29314a09339e8afdecab6481451b889d
Prabuddhi-05 <119497903+Prabuddhi-05@users.noreply.github.com>
2024-07-19 11:01:32 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 import os
 from glob import glob
 
-package_name = 'strawberry_data_collection'
+package_name = 'fisheye_camera_driver'
 
 setup(
     name=package_name,
@@ -23,7 +23,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-          'fisheye_camera_multiple = strawberry_data_collection.fisheye_camera_multiple:main',
+          'fisheye_camera_multiple = fisheye_camera_driver.fisheye_camera_multiple:main',
         ],
     },
 )
